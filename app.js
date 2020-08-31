a5d1be78142450ac771fdae15bad5ee352b2760b
Andrew Reynolds <andrew_reynolds@mac.com>
2020-08-30 18:33:48 -0700

    added require module and call to weatherstack api for current forecast

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const chalk = require("chalk");
 const request = require("request");
 const weatherURL = "http://api.weatherstack.com/current?access_key=335ba8dae0451c7062b9638a7c34dfa0&query=Vancouver";
 
-request({ url: weatherURL }, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(data.current);
+request({ url: weatherURL, json: true }, (error, response, body) => {
+  console.log(
+    chalk.inverse(`Today's Forcast for ${body.location.name}: ${body.location.localtime}
+  - ${body.current.weather_descriptions[0]}   
+  - It's currently ${body.current.temperature}°C out. There is ${body.current.precip} of percipitation.`)
+  );
 });
